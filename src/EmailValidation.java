80c5215c40addfb25bc339905f0dbb2c57d01628
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-03-11 22:06:22 +0530

    tested all the samples emails

--- src/EmailValidation.java
+++ src/EmailValidation.java
@@ -7,34 +7,23 @@ public class EmailValidation {
 	public static void main(String[] args) {
 		/**
 		 * Procedure
+		 * =====================================================
+		 * 1.Created object of class 1.Created object of class
+		 * 2.Created object of class
 		 * ====================================================
-		 *  1.Taking user input of email
-		 *  3.calling method to print true false
-		 *  2.created method to check email
-		 *  ==================================================
 		 */
 
 		/*
-		 * 1.Taking user input of email
+		 * 1.Created object of class
 		 */
-		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Example: xyz12@bl.co.in \n");
-		System.out.println("Enter the Email address : ");
-		String emailId = sc.nextLine();
+		SampleEmails check = new SampleEmails();
+
 		/*
-		 * 3.calling method to print true false
+		 * 2.Created object of class
 		 */
-		System.out.println(email(emailId));
+		check.testForEmails();
 
 	}
 
-	/*
-	 * 2.created method to check email min 3-char min 2-number minimum one @ minimum
-	 * one . min 3 char at last
-	 */
-	public static boolean email(String email) {
-		return email.matches("([a-z]+[0-9]+@bl[.]co[.]in)*");
-
-	}
 }
